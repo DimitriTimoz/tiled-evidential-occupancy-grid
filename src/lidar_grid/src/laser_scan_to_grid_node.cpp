2b85b2eb5a165d5baa4d3e953b2d0bd9639bc5e8
DimitriTimoz <dimitri.timoz@protonmail.com>
2024-03-07 17:53:48 +0100

    Sensor msg interface

--- src/lidar_grid/src/laser_scan_to_grid_node.cpp
+++ src/lidar_grid/src/laser_scan_to_grid_node.cpp
@@ -2,6 +2,23 @@
 **  Simple ROS Node
 **/
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+class LaserScanToGrid
+{
+public:
+  LaserScanToGrid(ros::NodeHandle& nh)
+  {
+    ar_sub_ = nh.subscribe<sensor_msgs::LaserScan>("laser_scan", 1, &LaserScanToGrid::visionCallback, this);
+  }
+
+  void visionCallback(const sensor_msgs::LaserScanConstPtr& msg)
+  {
+    ROS_INFO_STREAM(msg);
+  }
+
+  ros::Subscriber ar_sub_;
+};
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +27,7 @@ int main(int argc, char* argv[])
   // Create a ROS node handle
   ros::NodeHandle nh;
 
+  LaserScanToGrid laser_scan_to_grid(nh);
   ROS_INFO("Hello, World!");
 
   // Don't exit the program.
