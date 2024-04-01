81e53623f5ee6e27df0c92fad6bc942c94c60ace
Alix ANNERAUD <alix@anneraud.fr>
2024-04-01 19:59:32 +0200

    Replace CellState with BeliefMassFunction in EOGM

--- src/common/include/common/EOGM.h
+++ src/common/include/common/EOGM.h
@@ -3,30 +3,30 @@
 #include <vector>
 #include <map>
 #include "Point2D.h"
+#include "BeliefMassFunction.h"
 #include <nav_msgs/OccupancyGrid.h>
 
 using namespace std;
 
-enum class CellState
-{
-    OCCUPIED,
-    FREE,
-    UNKNOWN
-};
-
 class EOGM
 {
 public:
-    EOGM(vector<std::vector<unsigned int>> occupied, vector<std::vector<unsigned int>> free, float resolution);
+    EOGM() = default;
+    EOGM(unsigned int width, unsigned int height, float resolution);
+    EOGM(vector<std::vector<unsigned int>> occupied, vector<std::vector<unsigned int>> free, float resolution, float certainty = 0.75);
+    EOGM(nav_msgs::OccupancyGrid grid_msg, float certainty = 0.75);
     ~EOGM() = default;
 
     /// Get the grid as a map of points to cell states (occupied, free, unknown)
     /// @param rotation_matrix The rotation matrix of the robot according to the origin
     /// @param translation_vector The translation vector of the robot according to the origin
-    map<Point2D, CellState> getGrid(float rotation_matrix[2][2], float translation_vector[2]);
+    map<Point2D, BeliefMassFunction> getGrid(float rotation_matrix[2][2], float translation_vector[2]);
     nav_msgs::OccupancyGrid getOccupancyGrid();
 
+
+
 private:
-    vector<std::vector<CellState>> grid;
+    vector<std::vector<BeliefMassFunction>> grid;
+
     float resolution;
 };
--- src/common/src/EOGM.cpp
+++ src/common/src/EOGM.cpp
@@ -2,29 +2,52 @@
 #include <cmath>
 #include <ros/ros.h>
 
-EOGM::EOGM(vector<std::vector<unsigned int>> occupied, vector<std::vector<unsigned int>> free, float resolution) : resolution(resolution)
+EOGM::EOGM(unsigned int width, unsigned int height, float resolution) : resolution(resolution)
 {
-    this->grid = vector<std::vector<CellState>>(occupied.size(), vector<CellState>(occupied[0].size(), CellState::UNKNOWN));
+    this->grid = vector<std::vector<BeliefMassFunction>>(width / resolution, vector<BeliefMassFunction>(height / resolution, BeliefMassFunction()));
+}
+
+EOGM::EOGM(vector<std::vector<unsigned int>> occupied, vector<std::vector<unsigned int>> free, float resolution, float certainty) : resolution(resolution)
+{
+    this->grid = vector<std::vector<BeliefMassFunction>>(occupied.size(), vector<BeliefMassFunction>(occupied[0].size(), BeliefMassFunction()));
 
     for (int x = 0; x < occupied.size(); x++)
     {
         for (int y = 0; y < occupied[0].size(); y++)
+        
         {
             if (occupied[x][y] > 0)
-            {
-                this->grid[x][y] = CellState::OCCUPIED;
-            }
+                this->grid[x][y] = BeliefMassFunction(BeliefMassFunction::State::OCCUPIED, certainty);
             else if (occupied[x][y] < free[x][y])
+                this->grid[x][y] = BeliefMassFunction(BeliefMassFunction::State::FREE, certainty);
+        }
+    }
+}
+
+EOGM::EOGM(nav_msgs::OccupancyGrid grid_msg, float certainty) : resolution(grid_msg.info.resolution)
+{
+    this->grid = vector<std::vector<BeliefMassFunction>>(grid_msg.info.width, vector<BeliefMassFunction>(grid_msg.info.height, BeliefMassFunction()));
+
+    for (int x = 0; x < grid_msg.info.width; x++)
+    {
+        for (int y = 0; y < grid_msg.info.height; y++)
+        {
+            int index = x + y * grid_msg.info.width;
+            if (grid_msg.data[index] == 100)
             {
-                this->grid[x][y] = CellState::FREE;
+                this->grid[x][y] = BeliefMassFunction(BeliefMassFunction::State::OCCUPIED, certainty);
+            }
+            else if (grid_msg.data[index] == 0)
+            {
+                this->grid[x][y] = BeliefMassFunction(BeliefMassFunction::State::FREE, certainty);
             }
         }
     }
 }
 
-map<Point2D, CellState> EOGM::getGrid(float rotation_matrix[2][2], float translation_vector[2])
+map<Point2D, BeliefMassFunction> EOGM::getGrid(float rotation_matrix[2][2], float translation_vector[2])
 {
-    map<Point2D, CellState> grid_map;
+    map<Point2D, BeliefMassFunction> grid_map;
     Point2D translation((int)round(translation_vector[0]), (int)round(translation_vector[1]));
 
     for (int x = 0; x < this->grid.size(); x++)
@@ -55,12 +78,9 @@ nav_msgs::OccupancyGrid EOGM::getOccupancyGrid()
     vector<int8_t> data;
 
     for (int x = 0; x < this->grid.size(); x++)
-    {
         for (int y = 0; y < this->grid[0].size(); y++)
-        {
-            data.push_back(this->grid[x][y] == CellState::OCCUPIED ? 100 : (this->grid[x][y] == CellState::FREE ? 0 : 50));
-        }
-    }
+            data.push_back(int8_t(this->grid[x][y].getMass(BeliefMassFunction::State::OCCUPIED) * 100));
+        
 
     grid_msg.data = data;
 
