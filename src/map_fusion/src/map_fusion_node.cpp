75c69e5ebe900f87f8e020ba01bbe524896d8bc3
Alix ANNERAUD <alix@anneraud.fr>
2024-05-20 20:59:47 +0200

    Fix crazy shit bug

--- src/map_fusion/src/map_fusion_node.cpp
+++ src/map_fusion/src/map_fusion_node.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     ros::NodeHandle node_handle;
 
     // EvidentialGrid evidential_grid(node_handle, 0.05, "/rmp440le/odom", "/jaguar_robot/scan");
-    EvidentialGrid evidential_grid(node_handle, 40, 40, 0.25, "/robot/throttle/odom", "/robot/throttle/scan");  // For the Robotnik Vogui
+    EvidentialGrid evidential_grid(node_handle, 40, 40, 0.05, "/robot/throttle/odom", "/robot/throttle/scan");  // For the Robotnik Vogui
 
     ROS_INFO("Hello, I am a map_fusion_node.");
 
